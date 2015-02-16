fdf13b62f066435de0827019c8ea359ec3bf7ef2
Jack Burrell <jab743@nau.edu>
2015-02-15 20:52:02 -0700

    Made email form

---
+++ agency/forms.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.db import models
+from agency.modles import email
+
+class emailForm(forms.Form):
+	
+	class Meta:
+		model = email
+		fields = ['email', 'firstname', 'lastname']
+		
+	email = new forms.Email.Field(label='Your email, required=True)
+	firstname = forms.CharField(max_length=128)
+	lastname = forms.CharField(max_length=128)
+	
+
