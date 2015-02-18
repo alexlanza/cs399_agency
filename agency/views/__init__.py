0205098a8f43bb7bedc6d504f8908dadf789ab75
Alex Lanza <alexlanza@no-reply.com>
2015-02-18 02:27:48 -0700

    Added views to init
    not sure if this is necessary... but seems to work

--- agency/views/__init__.py
+++ agency/views/__init__.py
@@ -1,3 +1,4 @@
-from views_default import home
-from views_default import about
-from views_default import email
+from views_default import *
+from campaigns import *
+from email import *
+from mp3 import *
