b4e40a25dd8f8bdb4b4a69f80ee1285ee6ea246f
Alex Lanza <alexlanza@no-reply.com>
2015-02-18 02:31:01 -0700

    Email view now displays a page
    tried to fix this view.. but left the comments and made it so it would
    display email.html for now

--- agency/views/email.py
+++ agency/views/email.py
@@ -7,19 +7,19 @@ from django.http import HttpResponseRedirect
 from django.db import models
 from agency.forms import emailForm
 	
-# def email(request):
-#     if request.method == 'POST':
-# 	    form = Contest1Form(request.POST)
-# 	    if form.is_valid():
-# 		    x = email()
-# 		    x.firstname = form.cleaned_data["firstname"]
-# 			x.lastname = form.cleaned_data["lastname"]
-# 			x.email = form.cleaned_data["email"]
-# 		    x.save()
-# 		    return HttpResponseRedirect("/thanks")
-#     elif request.method == 'GET':
+def email(request):
+    return render(request, "email.html", {})
+#def email(request):
+#	if request.method == 'POST':
+#		emailForm = Contest1Form(request.POST)
+#	if form.is_valid():
+#		x = email()
+#		x.firstname = form.cleaned_data["firstname"]
+#		x.lastname = form.cleaned_data["lastname"]
+#		x.email = form.cleaned_data["email"]
+#		x.save()
+#		return HttpResponseRedirect("/thanks")
+#	elif request.method == 'GET':
 # 	    form = Contest1Form()
-#     else:
-# 	    return HttpResponseRedirect("/404/")
-#
-#     return render(request, "email.html", {"form" : form})
+#	else:
+#		return render(request, "email.html", {"form" : form})
