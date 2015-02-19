241fbacfc7583e87916507da78caf58b64f07b27
Alex Lanza <alexlanza@no-reply.com>
2015-02-19 00:44:15 -0700

    Created MtnDewPoll campaign
    replaced generic poll with mtndewpoll

---
+++ agency/admin.py
@@ -0,0 +1,4 @@
+from agency.models import MtnDewFlavorPoll
+from django.contrib import admin
+
+admin.site.register(MtnDewFlavorPoll)
--- agency/views/__init__.py
+++ agency/views/__init__.py
@@ -2,3 +2,4 @@ from views_default import *
 from campaigns import *
 from email import *
 from mp3 import *
+from mtndewpoll import *
