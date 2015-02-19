1092427538028025b99840b03b495815964ecec5
Michael Gilbert <drumz4jah@yahoo.com>
2015-02-19 01:42:03 -0700

    Improved mp3 models to include __str__

--- agency/models/mp3.py
+++ agency/models/mp3.py
@@ -4,10 +4,16 @@ from django.db import models
 class Mp3Question(models.Model):
     question_text = models.CharField(max_length=255)
     email = models.EmailField(max_length=254)
-    created = models.DateTimeField('date created')
+    created = models.DateTimeField('date created', blank=True, null=True)
 
+    def __str__(self):
+        return self.question_text
 
 class Mp3Choice(models.Model):
     question = models.ForeignKey(Mp3Question)
     choice_text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.choice_text
+
 
