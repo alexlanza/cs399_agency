adfac0336a27f91ba340c3b6886c5db38f7dee9a
Alex Lanza <alexlanza@no-reply.com>
2015-02-18 18:27:07 -0700

    Created a list of campaigns on campaign page
    Created a list of campaigns on campaign page and created links to the
    campaigns

--- agency/urls.py
+++ agency/urls.py
@@ -11,6 +11,6 @@ urlpatterns = patterns('',
     url(r'^campaigns$', 'agency.views.campaigns', name='campaigns'),
     url(r'^campaigns/mp3$', 'agency.views.mp3.mp3', name='mp3'),
     url(r'^campaigns/poll$', 'agency.views.poll', name='poll'),
-    url(r'^email$', 'agency.views.email.email', name='email'),
+    url(r'^campaigns/email$', 'agency.views.email.email', name='email'),
     url(r'^admin/', include(admin.site.urls)),
 )
