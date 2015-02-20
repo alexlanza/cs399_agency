56a7c420d8e44f204fd79fc64f881f8cdf06be99
Michael Gilbert <drumz4jah@yahoo.com>
2015-02-20 01:38:38 -0700

    Added methods to count total votes for mp3 poll

--- agency/views/mp3.py
+++ agency/views/mp3.py
@@ -9,9 +9,9 @@ from agency.models.mp3 import Mp3Question, Mp3Choice, Mp3CollectedData
 
 class Mp3Form(forms.Form):
     # list comprehension to get all of the choices for the first question, which has primary key=1
-    SERVICE_CHOICES = [(c.choice_text, c.choice_text) for c in Mp3Question.objects.get(pk=1).mp3choice_set.all()]
+    GENRE_CHOICES = [(c.choice_text, c.choice_text) for c in Mp3Question.objects.get(pk=1).mp3choice_set.all()]
     # all of the choices for question 2...
-    GENRE_CHOICES = [(c.choice_text, c.choice_text) for c in Mp3Question.objects.get(pk=2).mp3choice_set.all()]
+    SERVICE_CHOICES = [(c.choice_text, c.choice_text) for c in Mp3Question.objects.get(pk=2).mp3choice_set.all()]
     # creates dropdown html "select" boxes using the above 2 variables as the options for the dropdowns
     service = forms.ChoiceField(choices=SERVICE_CHOICES,
                                 widget=forms.Select(),
@@ -22,8 +22,23 @@ class Mp3Form(forms.Form):
     email = forms.EmailField(max_length=255, label="Please enter your email ")
 
 
+def _get_service_votes():
+    services = {"Amazon": 0, "Google": 0, "iTunes": 0, "Spotify": 0}
+    for service in services:
+        votes = Mp3CollectedData.objects.filter(service=service).count()
+        services[service] = votes
+    return services
+
+
+def _get_genre_votes():
+    genres = {"Rap": 0, "Rock": 0, "Country": 0, "Electronic": 0, "Other": 0}
+    for genre in genres:
+        votes = Mp3CollectedData.objects.filter(genre=genre).count()
+        genres[genre] = votes
+    return genres
+
+
 def mp3(request):
-    collected_data = ""
     if request.method == 'POST':
 
         form = Mp3Form(request.POST)
@@ -41,5 +56,6 @@ def mp3(request):
     else:
         return HttpResponseRedirect('/')
     # get all the data people have entered thus far in the Mp3CollectedData table
-    collected_data = Mp3CollectedData.objects.all()
-    return render(request, 'mp3.html', { 'form': form, 'collected_data': collected_data })
+    service_votes = _get_service_votes()
+    genre_votes = _get_genre_votes()
+    return render(request, 'mp3.html', { 'form': form, 'service_votes': service_votes, 'genre_votes': genre_votes })
