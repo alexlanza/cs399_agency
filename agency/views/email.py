a1e0116b41903db6e795ecdf97d8d3c840eec052
Jack Burrell <jab743@nau.edu>
2015-02-19 18:07:54 -0700

    Fixed up views
    Fixed up views to show up, also redirects to Campaigns page

--- agency/views/email.py
+++ agency/views/email.py
@@ -1,7 +1,7 @@
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render
-from agency.models import email
+from agency.models import email_model
 from django import forms
 from django.http import HttpResponseRedirect
 from django.db import models
@@ -9,17 +9,20 @@ from agency.forms import emailForm
 	
 def email(request):
     return render(request, "email.html", {})
-#def email(request):
-#	if request.method == 'POST':
-#		emailForm = Contest1Form(request.POST)
-#	if form.is_valid():
-#		x = email()
-#		x.firstname = form.cleaned_data["firstname"]
-#		x.lastname = form.cleaned_data["lastname"]
-#		x.email = form.cleaned_data["email"]
-#		x.save()
-#		return HttpResponseRedirect("/thanks")
-#	elif request.method == 'GET':
-# 	    form = Contest1Form()
-#	else:
-#		return render(request, "email.html", {"form" : form})
+	
+	
+def email(request):
+	if request.method == 'POST':
+		form = emailForm(request.POST)
+		if form.is_valid():
+			x = email_model()
+			x.firstname = form.cleaned_data["firstname"]
+			x.lastname = form.cleaned_data["lastname"]
+			x.email = form.cleaned_data["email"]
+			x.save()
+			return HttpResponseRedirect('/campaigns')
+	elif request.method == 'GET':
+ 	    form = emailForm()
+	else:
+		return HttpResponseRedirect("/campaigns")
+	return render(request, "email.html", {"form" : form})
