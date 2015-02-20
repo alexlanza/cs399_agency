f9d42ffa188038ce219a5fd0942ab5e470d70c49
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-02-19 21:35:32 -0700

    added form and request handling

--- agency/views/mtndewpoll.py
+++ agency/views/mtndewpoll.py
@@ -1,15 +1,38 @@
-from django.core import serializers
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.db import models
 from django import forms
 from agency.models.mtndewpoll import MtnDewFlavorPoll
+from django.contrib import messages
+from django.http import Http404
 
-class MtnDewPollForm(forms.ModelForm):
-    class Meta:
-        model = MtnDewFlavorPoll
-        fields = ['flavor']
-	
+
+class MtnDewPollForm(forms.Form):
+    # class Meta:
+    #     model = MtnDewFlavorPoll
+    #     fields = ['flavor']
+
+    flavor = forms.ChoiceField(
+        choices=[('Cherry', 'Cherry'), ('Pineapple', 'Pineapple'), ('Berry', 'Berry'),
+                 ('Mango', 'Mango')],
+        widget=forms.Select(),
+        label="Which flavor would you dew?")
+
+
 def mtndewpoll(request):
-    return render(request, "mtndewpoll.html", {'form': MtnDewPollForm})
+    if request.method == 'POST':
+        form = MtnDewPollForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Thanks for voting!')
+            flavor = form.cleaned_data['flavor']
+            print flavor
+            vote = MtnDewFlavorPoll(flavor=flavor)
+            vote.save()
+        else:
+            messages.success(request, 'Something went wrong!')
+
+        return render(request, "mtndewpoll.html", {'form': MtnDewPollForm()})
+
+    elif request.method == 'GET':
+        return render(request, "mtndewpoll.html", {'form': MtnDewPollForm()})
+    else:
+        raise Http404
