562adde1b38c60707baac64b74fdae90f4212b9b
Justin <justin.poehnelt@gmail.com>
2015-02-19 22:36:48 -0700

    added messages to email form

--- agency/views/email.py
+++ agency/views/email.py
@@ -1,13 +1,14 @@
-from django.core import serializers
-from django.http import HttpResponse
 from django.shortcuts import render
 from agency.models import Email
 from django import forms
-from django.http import HttpResponseRedirect
-from django.db import models
+from django.http import Http404
+from django.contrib import messages
 
 
 class EmailForm(forms.Form):
+    """
+    Basic Django form for Email model.
+    """
     class Meta:
         model = Email
         fields = ['email', 'first_name', 'last_name']
@@ -16,7 +17,13 @@ class EmailForm(forms.Form):
     first_name = forms.CharField(max_length=128, required=True)
     last_name = forms.CharField(max_length=128, required=True)
 
+
 def email(request):
+    """
+    Handles the email newsletter signup.
+    :param request:
+    :return:
+    """
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
@@ -25,9 +32,11 @@ def email(request):
             x.last_name = form.cleaned_data["last_name"]
             x.email = form.cleaned_data["email"]
             x.save()
-            return HttpResponseRedirect('/campaigns')
+            messages.success(request, 'Thanks for signing up! We promise not to spam you...')
+        else:
+            messages.success(request, 'Something went wrong...')
+        return render(request, "email.html", {"form": EmailForm()})
     elif request.method == 'GET':
-        form = EmailForm()
+        return render(request, "email.html", {"form": EmailForm()})
     else:
-        return HttpResponseRedirect("/campaigns")
-    return render(request, "email.html", {"form": form})
+        raise Http404()  # method not allowed
