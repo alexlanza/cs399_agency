dc4fa779f16ab2bc9e7fc62f872735025bdd487c
Justin <justin.poehnelt@gmail.com>
2015-02-19 22:29:27 -0700

    fixed poll campaign url and pep8

--- agency/urls.py
+++ agency/urls.py
@@ -2,15 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'main.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-	url(r'^$', 'agency.views.home', name='home'),
-    url(r'^about$', 'agency.views.about', name='about'),
-    url(r'^campaigns$', 'agency.views.campaigns', name='campaigns'),
-    url(r'^campaigns/mp3$', 'agency.views.mp3.mp3', name='mp3'),
-    url(r'^campaigns/mtndewpoll$', 'agency.views.mtndewpoll', name='mtndewpoll'),
-	url(r'^campaigns/email$', 'agency.views.email.email', name='email'),
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', 'agency.views.home', name='home'),
+                       url(r'^about$', 'agency.views.about', name='about'),
+                       url(r'^campaigns$', 'agency.views.campaigns', name='campaigns'),
+                       url(r'^campaigns/mp3$', 'agency.views.mp3.mp3', name='mp3'),
+                       url(r'^campaigns/mtndewpoll$', 'agency.views.mtndewpoll.mtndewpoll', name='mtndewpoll'),
+                       url(r'^campaigns/email$', 'agency.views.email.email', name='email'),
+                       url(r'^admin/', include(admin.site.urls)),
 )
