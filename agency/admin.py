e0c304a32799d6f2f25167cc4cc53ed9e63bc866
Jack Burrell <jab743@nau.edu>
2015-02-19 18:06:13 -0700

    Added form to admin

--- agency/admin.py
+++ agency/admin.py
@@ -1,4 +1,6 @@
 from agency.models import MtnDewFlavorPoll
+from agency.models import email_model
 from django.contrib import admin
 
 admin.site.register(MtnDewFlavorPoll)
+admin.site.register(email_model)
